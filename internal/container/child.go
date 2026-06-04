a2e14aa726d7c5fdfe8e4c95f2cfa12b444af4f0
Saksham <sakshamkumar1415@gmail.com>
2026-06-04 19:17:53 +0530

    setting namespaces

--- internal/container/child.go
+++ internal/container/child.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func Child(args []string) {
+
+	// setting hostname for this specific container
+	if err := syscall.Sethostname([]byte("o1-container")); err != nil {
+		fmt.Print("Error setting the hostname: %v\n", err)
+		os.Exit(1)
+	}
+
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
--- internal/container/run.go
+++ internal/container/run.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func Run(args []string) {
@@ -16,6 +17,10 @@ func Run(args []string) {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
+	}
 
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Host container exited with error: %v\n", err)
